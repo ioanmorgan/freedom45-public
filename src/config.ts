26096664399cd0eeaffd4c94f25f833ccfad942d
Ioan Morgan <ioanamorgan90@gmail.com>
2026-03-22 20:28:41 +0000

    Biz email added

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const CALENDLY_URL = 'https://calendly.com/ioanamorgan90/30min';
-export const CONTACT_EMAIL = 'contact@ioanmorgan.com';
+export const CONTACT_EMAIL = 'contact@freedom45.co.uk';
